b2f80d940309ad71d3d9a3f1fd82365834378500
TofflerDev <baraban.ms@gmail.com>
2017-05-11 21:42:10 +0300

    parse content of a page

--- scrapper.rb
+++ scrapper.rb
@@ -5,11 +5,10 @@ require 'watir'
 x = 1
 agent = Mechanize.new
 page = agent.get("http://onlinelibrary.wiley.com/doi/10.1111/jacf.1988.1.issue-1/issuetoc")
-while next_link = page.link_with(css: 'a.next') do
-    puts x
-    x +=1
-    page = next_link.click
-end
+#while next_link = page.link_with(css: 'a.next') do
+   p page.search(".citation")#.at("p:contains('DOI:')").text
+    #page = next_link.click
+#end
 
 #issues_links.each do |l|
  #   l.click
