f4802d229cbb8bb576f3986a9184258e98682b8d
TofflerDev <baraban.ms@gmail.com>
2017-05-12 00:53:11 +0300

    final commit

--- scrapper.rb
+++ scrapper.rb
@@ -1,19 +1,20 @@
 require 'mechanize'
-require 'date'
-require 'json'
-require 'watir'
-x = 1
+
+doi_numbers = []
+repeat = true
 agent = Mechanize.new
 page = agent.get("http://onlinelibrary.wiley.com/doi/10.1111/jacf.1988.1.issue-1/issuetoc")
-#while next_link = page.link_with(css: 'a.next') do
-   p page.search(".citation")#.at("p:contains('DOI:')").text
-    #page = next_link.click
-#end
-
-#issues_links.each do |l|
- #   l.click
-#end
-
-#volume_links = page.links_with(css: 'div.issue')
-
-#puts volume_links.inspect
+while repeat == true
+   page.search(".citation").search("p").each do |p|
+      if p.text.include? "DOI:"
+        arr = p.text.split('|')
+        doi_numbers << arr.last
+      end
+    end
+    if next_link = page.link_with(css: 'a.next')
+    page = next_link.click
+else
+    repeat = false
+    end
+end
+puts doi_numbers
