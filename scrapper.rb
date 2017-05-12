bd121882441782f6f2dd835ef472f9f090139eb9
TofflerDev <baraban.ms@gmail.com>
2017-05-13 01:42:37 +0300

    download all the art

--- scrapper.rb
+++ scrapper.rb
@@ -17,4 +17,16 @@ else
     repeat = false
     end
 end
-puts doi_numbers
+
+doi_numbers.each do |doi|
+
+  agent = Mechanize.new
+  page = agent.get("http://sci-hub.cc/#{doi.split(' ').last}")
+  link = page.search("#save").search('a').attribute('onclick').value.gsub('location.href=\'','')
+  link = link.gsub('?download=true\'','')
+  agent.pluggable_parser.default = Mechanize::Download
+  agent.get(link).save(File.join('./pdfs/', "#{doi}.pdf"))
+  puts "#{doi}.pdf saved"
+  sleep 600
+
+end
